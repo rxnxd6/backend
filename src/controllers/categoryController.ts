c90f550cb1646130d9b57d6fcf37dca7ad6bf802
DvOmar76 <zzomarzz6@gmail.com>
2023-11-22 19:58:41 +0300

    work on realtion in order

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -1,71 +1,68 @@
-import { NextFunction, Request, Response } from 'express'
-import Category from '../models/category'
 import ApiError from '../errors/ApiError'
+import {Request,Response,NextFunction} from 'express'
+import Order from '../models/order'
+import User from '../models/user'
 
-export const getCategories= async (req:Request, res:Response)=>{
-  const categories= await Category.find().exec()
-  res.status(200).json({
-    msg:"categories is returned",
-    categories:categories
+export const createOrder= async (req:Request, res:Response, next:NextFunction) => {
+  const { userId, products ,date } = req.body
+  const order = new Order({
+    userId,
+    products,
+    date
   })
-}
-export const getCategory= async (req:Request, res:Response ,next:NextFunction)=>{
-  const {categoryId}=req.params
-  // console.log(categoryId)
-  const category= await Category.findById(categoryId).exec()
-  if (category!=null) {
-    res.status(200).json({
-      msg: "category is returned",
-      category: category
+  const user= await User.findOne({ _id: userId } ).exec()
+  // console.log(user)
+  if (user!=null){
+    user.order.push(order)
+    await order.save()
+    await user.save()
+    res.status(201).json({
+      msg: 'done',
+      order: order,
     })
-  }else {
-    res.status(404).json({
-      msg: " not found categoryId in db"
-    })
-  }
-}
-export const createCategory= async  (req:Request, res:Response)=>{
-  // you should work on validation in future  remember  name is unique
-  const {name}=req.body
-  const category=new Category({
-    name
-  })
-  res.status(201).json({
-    msg: "created category",
-    category: category
-  })
-}
-export const deleteCategory= async (req:Request, res:Response,next:NextFunction) => {
-  const  { categoryId } = req.params
-  // isDelete['deletedCount']  if  1 => order is deleted
-  const isDelete=await Category.deleteOne({_id:categoryId}).exec()
-  // console.log(isDelete)
-  if (isDelete['deletedCount'] === 1){
-    res.status(204).json({
-      msg: 'category is deleted'
-    })
-  }else {
-    res.status(404).json('error')
-    next(ApiError.badRequest('not found categoryId in db '))
+  }else{
+    res.status(404).json(' error ')
+    next(ApiError.badRequest(' not found userID in db '))
     return
   }
 }
-export const updateCategory= async (req:Request, res:Response,next:NextFunction) => {
-  const {categoryId}=req.params
-  const {name}=req.body
+export const getOrders = async (req:Request, res:Response) => {
+  //.populate('products') in populate write your key not name of models
+  const orders = await Order.find().populate('userId')
+  res.status(200).json({
+    msg:"order is returned ",
+    orders:orders
+  })
+}
+export const deleteOrder = async (req:Request, res:Response,next:NextFunction) => {
+  const  { orderId } = req.params
+  // isDelete['deletedCount']  if  1 => order is deleted
+  const isDelete= await Order.deleteOne({_id:orderId}).exec()
+  // console.log(isDelete)
+  if (isDelete['deletedCount'] === 1){
+    res.status(204).json({
+      msg: 'order is deleted'
+    })
+  }else {
+    res.status(404).json('error')
+    next(ApiError.badRequest('not found orderId in db '))
+    return
+  }
+}
+export const getOrder= async (req:Request, res:Response) => {
+  const  { orderId } = req.params
 
-  // console.log(categoryId)
-  const category= await Category.findById(categoryId).exec()
-  if (category!=null) {
-    category.name=name
-    await category.save()
+  const order= await Order.findById(orderId).populate('products').populate('userId').exec()
+
+  if (order!=null){
     res.status(200).json({
-      msg: "category is returned",
-      category: category
+      msg:"order is returned ",
+      order:order
     })
   }else {
     res.status(404).json({
-      msg: " not found categoryId in db"
+      msg:"order not found ",
     })
   }
+  // res.json(order)
 }
