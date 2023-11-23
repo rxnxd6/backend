e3ea370d960dd1424b5251b9ef3a7f8c824ad9da
DvOmar76 <84674856+DvOmar76@users.noreply.github.com>
2023-11-23 10:53:42 +0300

    Update orderController.ts

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -6,13 +6,14 @@ import User from '../models/user'
 export const createOrder= async (req:Request, res:Response, next:NextFunction) => {
   const { userId, products ,date } = req.body
   const order = new Order({
+    userId,
     products,
     date
   })
   const user= await User.findOne({ _id: userId } ).exec()
   // console.log(user)
   if (user!=null){
-     user.order.push(order)
+    user.order.push(order)
     await order.save()
     await user.save()
     res.status(201).json({
@@ -20,22 +21,28 @@ export const createOrder= async (req:Request, res:Response, next:NextFunction) =
       order: order,
     })
   }else{
-    res.status(404).json('error')
-    next(ApiError.badRequest('not found userID in db '))
+    res.status(404).json(' error ')
+    next(ApiError.badRequest(' not found userID in db '))
     return
   }
 }
 export const getOrders = async (req:Request, res:Response) => {
-  const orders = await Order.find().populate(['products','userId'])
+  const page:number=Number(req.query.page)||1
+  const perPage:number=Number(req.query.perPage)||3
+  const category=req.query.category
+
+  //.populate('products') in populate write your key not name of models
+  const orders = await Order.find().populate('userId')
+
   res.status(200).json({
     msg:"order is returned ",
     orders:orders
   })
 }
-export const deleteOrder =async (req:Request, res:Response,next:NextFunction) => {
+export const deleteOrder = async (req:Request, res:Response,next:NextFunction) => {
   const  { orderId } = req.params
   // isDelete['deletedCount']  if  1 => order is deleted
-  const isDelete=await Order.deleteOne({_id:orderId}).exec()
+  const isDelete= await Order.deleteOne({_id:orderId}).exec()
   // console.log(isDelete)
   if (isDelete['deletedCount'] === 1){
     res.status(204).json({
@@ -50,7 +57,7 @@ export const deleteOrder =async (req:Request, res:Response,next:NextFunction) =>
 export const getOrder= async (req:Request, res:Response) => {
   const  { orderId } = req.params
 
-  const order= await Order.findById(orderId).exec()
+  const order= await Order.findById(orderId).populate('products').populate('userId').exec()
 
   if (order!=null){
     res.status(200).json({
