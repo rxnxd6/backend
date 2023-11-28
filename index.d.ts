9e91697787cd51d07a43264133f512107e7b0667
HussamMohammed7 <h.m.sharmani@gmail.com>
2023-11-28 18:19:39 +0300

    update some stuff

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,11 @@
 declare namespace Express {
   interface Request {
-    msg: string
-    user: {
-      id: string
+    validatedUser: {
       first_name: string
+      last_name: string
+      email: string
+      password: string
     }
+   
   }
 }
