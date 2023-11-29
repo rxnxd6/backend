996f179e74e21c0c27240840be93afb32cf30a5f
aseelcs <aseeelrm@gmail.com>
2023-11-29 16:05:07 +0300

    product validation

---
+++ src/middlewares/validateProduct.ts
@@ -0,0 +1,25 @@
+
+import { NextFunction, Request, Response } from 'express'
+import zod, { string, z, ZodError } from 'zod'
+import ApiError from '../errors/ApiError'
+
+
+export const validateProduct = (req: Request, res: Response, next: NextFunction) => {
+    const getSchema = z.object({
+        name: (zod.string().nonempty('name is required')).min(5).max(50),
+        description: zod.string().min(5).max(50).optional()
+    })
+
+    try {
+        getSchema.parse(req.body)
+        next()
+
+    } catch (error) {
+        const err = error
+        if (err instanceof ZodError) {
+            next(ApiError.badRequestValidation(err.errors))
+            return
+        }
+        next(ApiError.internal('Something went wrong'))
+    }
+}
--- src/routers/products.ts
+++ src/routers/products.ts
@@ -1,15 +1,16 @@
 import express from 'express'
 const router = express.Router()
 import { createProduct, deleteProduct, getProducts, getProductbyId, updateProduct } from '../controllers/productController'
+import { validateProduct } from '../middlewares/validateProduct'
 
 //Get product 
 router.get('/', getProducts)
 //Get product By id
 router.get('/:productId', getProductbyId)
 //Create new product 
-router.post('/', createProduct)
+router.post('/', validateProduct, createProduct)
 //Update new product 
-router.put('/:productId', updateProduct)
+router.put('/:productId', validateProduct, updateProduct)
 //Delte product 
-router.delete('/:productId', deleteProduct)
+router.delete('/:productId', validateProduct, deleteProduct)
 export default router
