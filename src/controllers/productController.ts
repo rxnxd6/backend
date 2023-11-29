bc7eb6aeae39fe68859f9211154dc47a98cd8145
Rxnxd <Renad.aleidii@gmail.com>
2023-11-29 13:34:38 +0300

    productPagination

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -5,31 +5,43 @@ import product from '../models/product'
 
 //Get ALL Products
 export const getProducts = async (req: Request, res: Response, next: NextFunction) => {
-  const products = await Product.find().populate('categories')
-  res.status(200).json(
-    {
-      msg: "products is returned ",
-      products: products
-    })
+  const page = Number(req.query.page) || 1
+  const perPage = Number(req.query.perPage) || 3
+  // const products = await Product.find().populate('categories')
+
+  const totalProduct = await Product.countDocuments()
+  const totalPages = Math.ceil(totalProduct / perPage)
+  const products = await Product.find()
+    .skip((page - 1) * perPage)
+    .limit(perPage)
+    .populate('categories')
+
+  res.status(200).json({
+    msg: 'products is returned ',
+    products:products,
+    page,
+    perPage,
+    totalProduct,
+    totalPages,
+  })
 }
 //Get product by id
 export const getProductbyId = async (req: Request, res: Response) => {
   const { productId } = req.params
   try {
-    const productbyId = await Product.findById({ _id: productId, })
+    const productbyId = await Product.findById({ _id: productId })
 
     res.status(200).json({
-      msg: "Product by Id",
-      productbyId: productbyId
+      msg: 'Product by Id',
+      productbyId: productbyId,
     })
-  }
-  catch (error) {
+  } catch (error) {
     console.error(error)
     res.status(500).json({ error: 'Internal Server Error' })
   }
 }
 
-//create  a product 
+//create  a product
 export const createProduct = async (req: Request, res: Response, next: NextFunction) => {
   const { name, description, quantity, price, image, variants, size, categories } = req.body
   const product = new Product({
@@ -40,7 +52,7 @@ export const createProduct = async (req: Request, res: Response, next: NextFunct
     image,
     variants,
     size,
-    categories
+    categories,
   })
   if (!name || !description || !price) {
     next(ApiError.badRequest('Name , Description and price are requried'))
@@ -51,18 +63,25 @@ export const createProduct = async (req: Request, res: Response, next: NextFunct
   res.status(201).json({
     msg: 'product is created',
     product: product,
-    category: categories
+    category: categories,
   })
 }
-//Update new product 
-export const updateProduct = async (req: Request, res: Response,) => {
-  const { productId } = req.params;
-  const { name, description, quantity, price, image, variants, size, categories } = req.body;
+//Update new product
+export const updateProduct = async (req: Request, res: Response) => {
+  const { productId } = req.params
+  const { name, description, quantity, price, image, variants, size, categories } = req.body
 
   const product = await Product.findByIdAndUpdate(
     productId,
     {
-      name: name, description: description, quantity: quantity, price: price, image: image, variants: variants, size: size, categories: categories
+      name: name,
+      description: description,
+      quantity: quantity,
+      price: price,
+      image: image,
+      variants: variants,
+      size: size,
+      categories: categories,
     },
     {
       new: true,
@@ -70,7 +89,7 @@ export const updateProduct = async (req: Request, res: Response,) => {
   )
   res.status(201).json({
     msg: 'product is updated',
-    product: product
+    product: product,
   })
 }
 //Delete a product
@@ -79,7 +98,7 @@ export const deleteProduct = async (req: Request, res: Response, next: NextFunct
   const isDelete = await Product.deleteOne({ _id: productId }).exec()
   if (isDelete['deletedCount'] === 1) {
     res.status(201).json({
-      msg: 'product is deleted'
+      msg: 'product is deleted',
     })
   } else {
     res.status(404).json('error')
