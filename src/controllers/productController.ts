9189157d21ced8b4f0eaf405ae15abdf7a4e6c2a
aseelcs <aseeelrm@gmail.com>
2023-12-03 15:03:18 +0300

    Sort & filter product

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -3,26 +3,53 @@ import ApiError from '../errors/ApiError'
 import { Request, Response, NextFunction } from 'express'
 import product from '../models/product'
 
+type Filter = {
+  category?: string
+  name?: { $regex: RegExp }
+}
+type SortOptions = {
+  sort?: 'asc' | 'desc' | { name: number }
+}
 //Get ALL Products
 export const getProducts = async (req: Request, res: Response, next: NextFunction) => {
   const page = Number(req.query.page) || 1
   const perPage = Number(req.query.perPage) || 3
   // const products = await Product.find().populate('categories')
+  const name = req.query.name
+  const sort = req.query.sort
+  const category = req.query.category
+  const filters: Filter = {}
+  const sortOptions: SortOptions = {}
+  if (category && typeof category === 'string') {
+    filters.category = category
+  }
 
+  if (name && typeof name === 'string') {
+    filters.name = { $regex: new RegExp(name, 'i') }
+  }
+  if (sort && typeof sort === 'string') {
+    if (sort === 'asc') {
+      sortOptions.sort = { name: 1 }
+    }
+    if (sort === 'desc') {
+      sortOptions.sort = { name: -1 }
+    }
+  }
   const totalProduct = await Product.countDocuments()
   const totalPages = Math.ceil(totalProduct / perPage)
-  const products = await Product.find()
+  const items = await Product.find(filters)
     .skip((page - 1) * perPage)
     .limit(perPage)
     .populate('categories')
 
   res.status(200).json({
     msg: 'products is returned ',
-    products: products,
+    Product,
     page,
     perPage,
     totalProduct,
     totalPages,
+    items,
   })
 }
 //Get product by id
@@ -95,10 +122,6 @@ export const updateProduct = async (req: Request, res: Response) => {
 //Delete a product
 export const deleteProduct = async (req: Request, res: Response, next: NextFunction) => {
   const { productId } = req.params
-
-  // await Product.deleteOne({
-  //   _id: productId,
-  // })
 
   try {
     const result = await Product.deleteOne({
